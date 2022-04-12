ce56583db37409ff3ce44784d3d54e17a5f9294c
Paulina <pn.1990@wp.pl>
2022-04-12 15:58:58 +0100

    created repo and add first practice task

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Task 1
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+*/
+
+function sumDigits(number) {
+ //convert number to an array
+ const digitsArray = Array.from(String(Math.abs(number)))
+   
+ //return absolute value(sum array by reduce method)
+ return number===0? 0 : digitsArray.reduce((add1,add2)=> Number(add1)+Number(add2))
+}
+console.log(sumDigits(-126))
