665eeb18819ad8925a72c9f2d1324881e93487c1
Paulina <paulina.justyna.niedzielska@gmail.com>
2022-04-14 17:06:15 +0100

    added task 3

--- app.js
+++ app.js
@@ -47,3 +47,29 @@ function printerError(s) {
 }
 
 console.log(printerError("hithere"))
+
+/* Task 3
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+*/
+
+function capitalize(s){
+  let case1 = ""
+  let case2 = ""
+  for(let i = 0; i < s.length; i++){
+    if(i%2 === 0){
+      case1 += s[i].toUpperCase()
+      case2 += s[i].toLowerCase()
+    } else{
+      case1 += s[i].toLowerCase()
+      case2 += s[i].toUpperCase()
+    }
+  } 
+  return [case1,case2];
+};
+console.log(capitalize("abcdef"))
