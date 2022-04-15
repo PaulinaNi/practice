5046f859034e0217d1ae604d6f5af0df614c8a02
Paulina <paulina.justyna.niedzielska@gmail.com>
2022-04-15 05:12:28 +0100

    task 5 added

--- app.js
+++ app.js
@@ -89,3 +89,27 @@ function addBinary(a,b) {
 return sum.toString(2)
 }
 console.log(addBinary(2,6))
+
+/* Task 5
+https://www.codewars.com/kata/541c8630095125aba6000c00
+
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+ 16  -->  1 + 6 = 7
+  942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+  132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+  493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+
+function digital_root(n){
+  let digitalRootArray = Array.from(String(n)).map(singleNum =>Number(singleNum))
+  for(let i = 0; digitalRootArray.length > 1; i++ ){
+    digitalRootArray = Array.from(String(digitalRootArray.reduce((sum, added)=> sum+added))).map(singleNum =>Number(singleNum))
+    
+  }
+  return digitalRootArray[0]
+}
+console.log(digital_root(456))
